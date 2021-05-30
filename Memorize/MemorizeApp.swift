598d45f62d3228061db149834d0551f184fad0fa
Iana Sanduta <iana.sandutsa@gmail.com>
2021-05-30 12:37:24 -0600

    Created an empty project

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Iana Sanduta on 5/30/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
